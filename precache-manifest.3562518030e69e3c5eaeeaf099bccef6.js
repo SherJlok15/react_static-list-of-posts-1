04b6efa2c57a1a5a22c4de64e46628824ddb14a4
Fandorin <SherJlok15@gmail.com>
2019-06-11 13:40:20 +0300

    Updates

---
+++ precache-manifest.3562518030e69e3c5eaeeaf099bccef6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e83e440451a6200b7c237343cd0d706",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "4479594db040d8ac0cb2",
+    "url": "/myapp/static/css/main.0860aaf5.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/myapp/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "4479594db040d8ac0cb2",
+    "url": "/myapp/static/js/main.38768a25.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);
